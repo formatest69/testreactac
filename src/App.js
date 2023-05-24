a1561b777544198a54184812ad0464c7ab74e747
formatest69 <formatest69@gmail.com>
2023-05-24 16:01:22 +0200

    as
    as

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          RAYAN LAFFI <code>src/App.js</code> and save to reload.
+          TEST MAJ <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
