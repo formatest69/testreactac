f9386c31375a49b524a99b41b0a28ecb4d4ae903
formatest69 <formatest69@gmail.com>
2023-05-24 16:18:04 +0200

    as
    saas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST MAJ 2222222 <code>src/App.js</code> and save to reload.
+          12345 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
